8fad0cb7541903e25508190947fbaedc77e17004
Esskee <77013627+Esskee@users.noreply.github.com>
2022-04-27 12:45:55 +0100

    confg clean

--- config.py
+++ config.py
@@ -2,11 +2,4 @@
 
 class basevars():
 
-    host = 'hd-rds-interviewees-cluster.cluster-cxqp4co8nabg.eu-west-1.rds.amazonaws.com'
-    user = 'interview_user'
-    port = 5432
-    password = '*9zng$kv#g>L[`>{6beX.}U,H&p^^!Vv'
-    database = 'external_demos'
-    sslmode = 'require'
-
-    conn_string = f'postgresql://{user}:{password}@{host}:{port}'
+    
